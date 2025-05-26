950174e4b673aef774c252ca5c21d4d8f01041cc
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-05-26 19:34:29 +0200

    implementacion de OAuth2 cliente OK

--- tienda/views.py
+++ tienda/views.py
@@ -8,8 +8,13 @@ def index(request):
     return render(request, "index.html")
 
 def listar_pieza_api(request):
+    headers= {
+        'Authorization': 'Bearer 2Jrcl9KE2TVxJ70AGr4YYjLR2UBHdD'
+    }
+    
     #datos de la API
-    response = requests.get('http://0.0.0.0:8000/api/v1/listar_piezas/')
+    response = requests.get('http://0.0.0.0:8000/api/v1/listar_piezas/',
+                            headers=headers)
     
     #transformar los datos a un formato JSON
     listar_pieza_api = response.json()
