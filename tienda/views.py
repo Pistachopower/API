9a4247ee6e2936f4e178f92a2ad87ea5bc3d091e
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-05-26 18:40:32 +0200

    cliente funcionando con api. Falta arreglar bootstrap

---
+++ tienda/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+import requests 
+from django.core import serializers
+
+
+def index(request):
+    return render(request, "index.html")
+
+def listar_pieza_api(request):
+    #datos de la API
+    response = requests.get('http://0.0.0.0:8000/api/v1/listar_piezas/')
+    
+    #transformar los datos a un formato JSON
+    listar_pieza_api = response.json()
+    
+    return render(request, 'piezas/listar_pieza_api.html', {'listar_pieza_api': listar_pieza_api})
+    
+    
+
